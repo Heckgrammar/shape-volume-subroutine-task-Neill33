f19d9158870d0f0690f11550ce7c8449c05af40a
Neil Dhiman <21NDhiman@heckgrammar.co.uk>
2025-03-31 12:20:05 +0100

    hey

--- ShapeAreaCalculatorTask/Program.cs
+++ ShapeAreaCalculatorTask/Program.cs
@@ -11,9 +11,30 @@
             // Pentagon
             // Octagon
         }
-        static int areaOfRectangle()//Add the parameters
+        static double AreaOfCircle(double radius)
         {
-
+            return 3.1415 * radius * radius;
         }
+
+        static double AreaOfTriangle(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+        static double AreaOfRectangle(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double AreaOfPentagon(double sideLength, double apothem)
+        {
+            return (5 * sideLength * apothem) / 2;
+        }
+
+        static double AreaOfOctagon(double sideLength)
+        {
+            return 2 * (1 + Math.Sqrt(2)) * sideLength * sideLength;
+        }
+
     }
 }
